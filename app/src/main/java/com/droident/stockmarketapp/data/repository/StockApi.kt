ec7010340196fffec4e54190d41558d9435febcf
abdulmunam <munamdev@gmail.com>
2022-11-17 23:41:50 +0000

    "Company Listing ViewModel class done"

--- app/src/main/java/com/droident/stockmarketapp/data/repository/StockApi.kt
+++ app/src/main/java/com/droident/stockmarketapp/data/repository/StockApi.kt
@@ -7,7 +7,12 @@ import retrofit2.http.Query
 interface StockApi {
 
     @GET("query?function=LISTING_STATUS")
-    suspend fun getListing(@Query("apikey") apiKey:String):ResponseBody
+    suspend fun getListing(@Query("apikey") apiKey:String= API_KEY)
+    :ResponseBody
 
+    companion object{
+        const val API_KEY =""
+        const val BASE_URL=""
+    }
 
 }
